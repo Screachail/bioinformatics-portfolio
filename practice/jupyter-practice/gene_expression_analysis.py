011030e1ac980daba10e747d6f773c7e04684d84
Screachail <kacper.szafraniec.sonas@gmail.com>
2025-10-28 19:34:32 +0100

    Add Jupyter notebook with interactive gene expression analysis

---
+++ practice/jupyter-practice/gene_expression_analysis.py
@@ -0,0 +1,9 @@
+# Gene Expression Analysis
+# Mini projekt w Jupyter Notebook
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+print("📊 Biblioteki załadowane pomyślnie!")
